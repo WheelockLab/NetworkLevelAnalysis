47d11c82a93938f0d22bf7465f65771572ff8f8a
Harrison Clark <clark.h@wustl.edu>
2023-08-01 12:13:19 -0500

    Rename Welch's T (custom) to Welch's T, as it now produces identical results to builtin ttest2 welch's

--- +nla/+edge/+test/WelchT.m
+++ +nla/+edge/+test/WelchT.m
@@ -3,7 +3,7 @@ classdef WelchT < nla.edge.BaseTest
     %   Differs slightly from built-in Welch's ttest (errors ~1e-13) but
     %   runs about 4x faster.
     properties (Constant)
-        name = "Welch's T (Custom)"
+        name = "Welch's T"
         coeff_name = "Welch's T"
     end
     
