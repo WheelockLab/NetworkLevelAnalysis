b171d31f7a9fe8a32b3f1e9efdb0d2a183cc257d
Jim Pollaro <jim.pollaro@gmail.com>
2024-02-09 14:53:42 -0600

    formatting and comments

--- +nla/+net/CohenDTest.m
+++ +nla/+net/CohenDTest.m
@@ -1,5 +1,10 @@
 classdef CohenDTest < handle
     %COHENDTEST Cohen's D Test for network tests
+    % This Cohen's D test is run for all of the tests
+    % Input:
+    %   edge_test_results: Results from the edge tests
+    %   network_atlas: Network Atlas
+    %   result_object: This is a NetworkTestResult object. This needs to be passed in, and then it will be returned.
 
     properties (Constant)
         name = "Cohen's D Test"
@@ -16,11 +21,14 @@ classdef CohenDTest < handle
 
             for row = 1:number_of_networks
                 for column = 1:row
+
                     network_rho = edge_test_results.coeff.get(network_atlas.nets(row).indexes,...
                         network_atlas.nets(column).indexes);
+                    
                     single_sample_d = abs(mean(network_rho)) / std(network_rho);
                     d = abs((mean(network_rho) - mean(edge_test_results.coeff.v)) / sqrt(((std(network_rho).^2)) +...
                         (std(edge_test_results.coeff.v).^2)));
+                    
                     result_object.no_permutations.d.set(row, column, single_sample_d);
                     result_object.within_network_pair.d.set(row, column, single_sample_d);
                     result_object.full_connectome.d.set(row, column, d);
