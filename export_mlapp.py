2a507ad8f3efdc7a5fb1cb2411dfb716fd920a6e
Jim Pollaro <30908497+pollaro@users.noreply.github.com>
2024-03-18 10:18:14 -0500

    Switched to python and it works first try!
    Update export-mlapp-files.yml
    
    added license variable from mathworks
    specified version
    cache install for speed
    
    Update export-mlapp-files.yml
    
    added license variable from mathworks
    specified version
    cache install for speed
    
    Update export-mlapp-files.yml
    
    added build matrix
    fixed version number error
    
    Update export-mlapp-files.yml
    
    added toolboxes
    
    Update export-mlapp-files.yml
    
    fixed spaces to underscores in toolboxes
    
    Update export-mlapp-files.yml
    
    bug fix
    
    Update export-mlapp-files.yml
    
    changing commands to run from command line
    
    Update export-mlapp-files.yml
    
    trying a script
    
    Update export-mlapp-files.yml
    
    i hate matlab
    
    add script
    
    still trying to export crap
    
    Update export-mlapp-files.yml
    
    more
    
    really getting sick of this
    
    Update export-mlapp-files.yml
    
    Update export-mlapp-files.yml
    
    added exported file hoping it fixes the github action
    
    Update export-mlapp-files.yml
    
    added build matrix
    fixed version number error
    
    Update export-mlapp-files.yml
    
    added toolboxes
    
    Update export-mlapp-files.yml
    
    fixed spaces to underscores in toolboxes
    
    Update export-mlapp-files.yml
    
    bug fix
    
    Update export-mlapp-files.yml
    
    changing commands to run from command line
    
    Update export-mlapp-files.yml
    
    trying a script
    
    Update export-mlapp-files.yml
    
    i hate matlab
    
    add script
    
    still trying to export crap
    
    really getting sick of this
    
    Update export-mlapp-files.yml
    
    change export script to use unzip. deal with the xml header and footer later
    
    stupid typo
    
    hoping this actually works. created blank files first
    
    add python script
    
    Update export-mlapp-files.yml
    
    Switch to real language
    
    remove intermediate files
    
    remove intermediate files

---
+++ export_mlapp.py
@@ -0,0 +1,16 @@
+import os, zipfile
+
+for filename in [('./NLA_GUI.mlapp', './NLA_GUI_exported.m'), ('./NLAResult.mlapp', './NLAResult_exported.m')]:
+    with zipfile.ZipFile(filename[0]) as zip_object:
+        zip_object.extract('matlab/document.xml')
+    with open('matlab/document.xml') as filedata:
+        data = filedata.read().splitlines(True)
+
+    classdef_index = data[0].find('classdef')
+    with open(filename[1], 'w') as newfile:
+        newfile.write(data[0][classdef_index:])
+        newfile.writelines(data[1:-1])
+        newfile.write('end')
+
+os.remove('./matlab/document.xml')
+os.rmdir('./matlab')
