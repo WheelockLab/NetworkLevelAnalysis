6071b36c88719bc0cc5f882d038821569afa5b85
Jim Pollaro <jim.pollaro@gmail.com>
2024-04-09 12:36:05 -0500

    some formatting

--- +nla/+gfx/+plots/MatrixPlot.m
+++ +nla/+gfx/+plots/MatrixPlot.m
@@ -49,7 +49,8 @@ classdef MatrixPlot < handle
         colorbar_offset = 15; % Offset of the colorbar
         colorbar_text_w = 50; % Width of label on colorbar
         legend_offset = 5; % Offset of the Legend
-        colormap_choices = {"Parula", "Turbo", "HSV", "Hot", "Cool", "Spring", "Summer", "Autumn", "Winter", "Gray", "Bone", "Copper", "Pink"}; % Colorbar choices
+        colormap_choices = {"Parula", "Turbo", "HSV", "Hot", "Cool", "Spring", "Summer", "Autumn", "Winter", "Gray",...
+            "Bone", "Copper", "Pink"}; % Colorbar choices
     end
 
     methods
@@ -101,8 +102,8 @@ classdef MatrixPlot < handle
             
             parse(matrix_input_parser, figure, name, matrix, networks, figure_size, varargin{:});
             properties = {'figure', 'name', 'matrix', 'networks', 'figure_size', 'network_clicked_callback',...
-                'marked_networks', 'figure_margins', 'draw_legend', 'draw_colorbar', 'color_map', 'lower_limit', 'upper_limit',...
-                'x_position', 'y_position', 'discrete_colorbar', 'plot_scale'};
+                'marked_networks', 'figure_margins', 'draw_legend', 'draw_colorbar', 'color_map', 'lower_limit',...
+                'upper_limit', 'x_position', 'y_position', 'discrete_colorbar', 'plot_scale'};
             for property = properties
                 obj.(property{1}) = matrix_input_parser.Results.(property{1});
                 if property{1} == "marked_networks"
@@ -128,7 +129,8 @@ classdef MatrixPlot < handle
 
             % initialization of the data that's going to become the image 
             image_data = NaN(dimensions("image_height"), dimensions("image_width"), 3);
-            obj.image_display = image(obj.axes, image_data, 'XData', [1 obj.axes.Position(3)], 'YData', [1 obj.axes.Position(4)]);
+            obj.image_display = image(obj.axes, image_data, 'XData', [1 obj.axes.Position(3)],...
+                'YData', [1 obj.axes.Position(4)]);
             % add callback for clicking on image
             obj.addCallback(obj.image_display);
 
@@ -509,7 +511,8 @@ classdef MatrixPlot < handle
 
             if obj.discrete_colorbar
                 number_of_ticks = double(upper_value - lower_value);
-                display_colormap = obj.color_map(floor((size(obj.color_map, 1) - 1) * [0:number_of_ticks] ./ number_of_ticks) + 1, :);
+                display_colormap = obj.color_map(floor(...
+                    (size(obj.color_map, 1) - 1) * [0:number_of_ticks] ./ number_of_ticks) + 1, :);
                 display_colormap = repelem(display_colormap, 2, 1);
                 display_colormap = display_colormap(2:((number_of_ticks + 1) * 2 - 1), :);
                 colormap(obj.axes, display_colormap);
@@ -525,7 +528,9 @@ classdef MatrixPlot < handle
 
             labels = {};
             for tick = ticks
-                labels{tick + 1} = sprintf("%.2g", lower_value + (tick * ((double(upper_value - lower_value) / number_of_ticks))));
+                labels{tick + 1} = sprintf(...
+                    "%.2g", lower_value + (tick * ((double(upper_value - lower_value) / number_of_ticks)))...
+                );
             end
             obj.color_bar.TickLabels = labels;
             
@@ -557,11 +562,11 @@ classdef MatrixPlot < handle
             d = figure('WindowStyle', 'normal', "Units", "pixels", 'Position', [source.Position(1), source.Position(2),...
                 source.Position(3) * 15, source.Position(4)/ 1.75]);
             % These are the boxes that are the upper and lower end of the scale
-            upper_limit_box = uicontrol('Style', 'edit', "Units", "pixels", 'Position', [90, d.Position(4) - 30, 100, 30], "String",...
-                obj.upper_limit);
+            upper_limit_box = uicontrol('Style', 'edit', "Units", "pixels",...
+                'Position', [90, d.Position(4) - 30, 100, 30], "String", obj.upper_limit);
             upper_limit_box.Position(4) = upper_limit_box.FontSize * 2;
-            lower_limit_box = uicontrol('Style', 'edit', "Units", "pixels", 'Position', [90, upper_limit_box.Position(2) - 30, 100, 30], "String",...
-                obj.lower_limit); 
+            lower_limit_box = uicontrol('Style', 'edit', "Units", "pixels",...
+                'Position', [90, upper_limit_box.Position(2) - 30, 100, 30], "String", obj.lower_limit); 
             lower_limit_box.Position(4) = lower_limit_box.FontSize * 2;
             uicontrol('Style', 'text', 'String', 'Upper Limit', "Units", "pixels", 'Position',...
                 [upper_limit_box.Position(1) - 80, upper_limit_box.Position(2) - 2, 80, upper_limit_box.Position(4)]);
@@ -585,8 +590,10 @@ classdef MatrixPlot < handle
             % Color Map selector
             % Adapted from colormap-dropdown: https://www.mathworks.com/matlabcentral/fileexchange/43659-colormap-dropdown-menu
             
-            uicontrol("Style", "text", "string", "Colormaps", "Units", "pixels", "Position", [10, scaleBaseButtons.Position(2) - 45, 80, 25]);
-            color_map_select = uicontrol('Style', 'popupmenu', 'Position', [100, scaleBaseButtons.Position(2) - 45, 250, 30]);
+            uicontrol("Style", "text", "string", "Colormaps", "Units", "pixels",...
+                "Position", [10, scaleBaseButtons.Position(2) - 45, 80, 25]);
+            color_map_select = uicontrol('Style', 'popupmenu',...
+                'Position', [100, scaleBaseButtons.Position(2) - 45, 250, 30]);
             initial_colors = 16;
             colormap_html = [];
             for colors = 1:numel(obj.colormap_choices)
@@ -594,7 +601,8 @@ classdef MatrixPlot < handle
                 CData = colormap_function(initial_colors);
                 new_html = '<HTML>';
                 for color_iterator = 1:initial_colors
-                    hex_code = nla.gfx.rgb2hex([CData(color_iterator, 1), CData(color_iterator, 2), CData(color_iterator, 3)]);
+                    hex_code = nla.gfx.rgb2hex([CData(color_iterator, 1), CData(color_iterator, 2),...
+                        CData(color_iterator, 3)]);
                     new_html = [new_html '<FONT bgcolor="' hex_code ' "color="' hex_code '">__</FONT>'];
                 end
                 %new_html = new_html(1:end-2);
@@ -623,7 +631,8 @@ classdef MatrixPlot < handle
             discrete_colors = NetworkResultPlotParameter().default_discrete_colors;
             color_map = get(color_map_select, "Value");
             if get(get(button_group, "SelectedObject"), "String") == "Linear"
-                obj.color_map = NetworkResultPlotParameter.getColormap(discrete_colors, get(upper_limit_box, "String"), obj.colormap_choices{color_map});
+                obj.color_map = NetworkResultPlotParameter.getColormap(discrete_colors, get(upper_limit_box, "String"),...
+                    obj.colormap_choices{color_map});
             else
                 obj.color_map = NetworkResultPlotParameter.getLogColormap(discrete_colors, obj.matrix,...
                     get(upper_limit_box, "String"). obj.colormap_choices{color_map});
