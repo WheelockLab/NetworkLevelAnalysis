dbeec9720658f61f4463df041debd60c0ab2f6f1
Jim Pollaro <jim.pollaro@gmail.com>
2024-04-11 11:16:47 -0500

    removed old network test folder

--- +nla/+net3/BaseSigTest.m
+++
@@ -1,9 +0,0 @@
-classdef BaseSigTest < nla.net.BaseTest
-    methods
-        function obj = BaseSigTest()
-            import nla.* % required due to matlab package system quirks
-            obj@nla.net.BaseTest();
-        end
-    end
-end
-
