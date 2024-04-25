24550dc3f3f71b24cd4b18911bc708f17721cb85
Jim Pollaro <30908497+pollaro@users.noreply.github.com>
2024-04-25 13:31:41 -0500

    [NET-161] remove "Import nla.*" (#16)
    * Fix bug in merge function of PermBase when taking single result. Now takes either single result or cell array
    
    * NET53 ChordPlot Class (#5)
    
    * network level chords working
    
    * remove two unneeded files
    
    * Attempting to get edge chord plotting working
    
    * chordplot used in all chord plotting instead of old function
    
    * Comments and documentation of chordplot
    
    * just some comment refining
    
    * remove commented lines
    
    * Create export-mlapp-files.yml
    
    * NET-149 Save figures as svg (#6)
    
    * add svg option to save figure
    
    * remove useless file
    
    * remove comment marks
    
    * Switched to python and it works first try!
    
    Update export-mlapp-files.yml
    
    added license variable from mathworks
    specified version
    cache install for speed
    
    Update export-mlapp-files.yml
    
    added license variable from mathworks
    specified version
    cache install for speed
    
    Update export-mlapp-files.yml
    
    added build matrix
    fixed version number error
    
    Update export-mlapp-files.yml
    
    added toolboxes
    
    Update export-mlapp-files.yml
    
    fixed spaces to underscores in toolboxes
    
    Update export-mlapp-files.yml
    
    bug fix
    
    Update export-mlapp-files.yml
    
    changing commands to run from command line
    
    Update export-mlapp-files.yml
    
    trying a script
    
    Update export-mlapp-files.yml
    
    i hate matlab
    
    add script
    
    still trying to export crap
    
    Update export-mlapp-files.yml
    
    more
    
    really getting sick of this
    
    Update export-mlapp-files.yml
    
    Update export-mlapp-files.yml
    
    added exported file hoping it fixes the github action
    
    Update export-mlapp-files.yml
    
    added build matrix
    fixed version number error
    
    Update export-mlapp-files.yml
    
    added toolboxes
    
    Update export-mlapp-files.yml
    
    fixed spaces to underscores in toolboxes
    
    Update export-mlapp-files.yml
    
    bug fix
    
    Update export-mlapp-files.yml
    
    changing commands to run from command line
    
    Update export-mlapp-files.yml
    
    trying a script
    
    Update export-mlapp-files.yml
    
    i hate matlab
    
    add script
    
    still trying to export crap
    
    really getting sick of this
    
    Update export-mlapp-files.yml
    
    change export script to use unzip. deal with the xml header and footer later
    
    stupid typo
    
    hoping this actually works. created blank files first
    
    add python script
    
    Update export-mlapp-files.yml
    
    Switch to real language
    
    remove intermediate files
    
    remove intermediate files
    
    * Add Myers Dec2023 50 Pct Thresh Network Atlas
    
    * Hotfix - Run edge/net perms together, don't save all edgePerm coeffs/p vals for all permutations
    
    * Update export-mlapp-files.yml
    
    * Exported mlapp files
    
    * removed as many import nla.* as I could find and attempted to wrap lines longer than 120
    
    * Exported mlapp files
    
    * found a few places for nla.helpers
    
    * fixed some missed imports
    
    * remove previously deleted files
    
    * Exported mlapp files
    
    ---------
    
    Co-authored-by: Andy <me@something.com>
    Co-authored-by: Github Action <action@github.com>

--- +nla/genTests.m
+++ +nla/genTests.m
@@ -2,13 +2,12 @@ function tests = genTests(subpackage)
     %GENTESTS Generate cell array containing all tests in given subpackage
     %   subpackage: dot-seperated subpackage name within NLA namespace, eg.
     %       'net.test' for net-level tests
-    import nla.* % required due to matlab package system quirks
-    root_path = findRootPath();
-    relative_path = strrep(subpackage, '.', '/+');
-    path_to = [root_path '+nla/+' relative_path];
-    network_tests_struct = dir(path_to);
-    network_test_folder_contents = {network_tests_struct.name};
-    network_test_filenames = network_test_folder_contents(~[network_tests_struct.isdir]);
+    root_path = nla.findRootPath();
+    rel_path = strrep(subpackage, '.', '/+');
+    path_to = [root_path '+nla/+' rel_path];
+    net_test_struct = dir(path_to);
+    net_test_folder_fnames = {net_test_struct.name};
+    net_test_fnames = net_test_folder_fnames(~[net_test_struct.isdir]);
 
     tests = {};
     for i = 1:numel(network_test_filenames)
