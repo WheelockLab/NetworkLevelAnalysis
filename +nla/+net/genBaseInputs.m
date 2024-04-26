ed4bb124ba87c058d4f79f4a79b1be8edd23a967
Jim Pollaro <jim.pollaro@gmail.com>
2024-04-26 13:46:40 -0500

    fix deleted file

---
+++ +nla/+net/genBaseInputs.m
@@ -0,0 +1,8 @@
+function inputs = genBaseInputs()
+    %GENBASEINPUTS Generate struct of required network-level inputs with
+    %   reasonable default values
+
+    inputs = struct('nonpermuted', true, 'full_conn', true, 'within_net_pair', true, 'prob_plot_method',...
+        nla.gfx.ProbPlotMethod.DEFAULT, 'ranking_method', nla.RankingMethod.P_VALUE, 'edge_chord_plot_method',...
+        nla.gfx.EdgeChordPlotMethod.PROB, 'fdr_correction', nla.net.mcc.Bonferroni(), 'd_thresh_chord_plot', true);
+end
