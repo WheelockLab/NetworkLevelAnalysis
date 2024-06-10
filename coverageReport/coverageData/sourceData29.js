8cdf162d9655788332a42a565e1e5308b93b6b65
Jim Pollaro <jim.pollaro@gmail.com>
2024-06-10 15:58:45 -0500

    rm coveragereport from local

--- coverageReport/coverageData/sourceData29.js
+++
@@ -1,1 +0,0 @@
-var sourceData29 = {"FileContents":["function z = fisherR2Z(r)","    %FISHERR2Z Transforms r-value data into z-value data via the","    % transform: z = 1/2 ln([1+r]/[1-r]) = arctanh(r)","    % z = arctanh(r);","    z = 0.5 .* (log(1 + r) - log(1 - r));","    z(~isfinite(z)) = 0;"],"CoverageData":{"CoveredLineNumbers":[5,6],"UnhitLineNumbers":[],"HitCount":[0,0,0,0,3,3]}}
