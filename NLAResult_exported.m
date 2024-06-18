e5e8fe588791984f68c51f156b22f8d17b8b9bbc
Github Action <action@github.com>
2024-06-18 18:46:44 +0000

    Exported mlapp files

--- NLAResult_exported.m
+++ NLAResult_exported.m
@@ -99,7 +99,7 @@ classdef NLAResult < matlab.apps.AppBase
                     end
                 end
                 
-                if app.net_input_struct.full_conn
+                if app.net_input_struct.full_connectome
                     root = app.createNode(app.ResultTree, 'Full connectome');
                     for i = 1:size(app.results.permutation_network_test_results, 2)
                         result = app.results.permutation_network_test_results{i};
@@ -111,7 +111,7 @@ classdef NLAResult < matlab.apps.AppBase
                     end
                 end
                 
-                if app.net_input_struct.within_net_pair
+                if app.net_input_struct.within_network_pair
                     root = app.createNode(app.ResultTree, 'Within Net-pair');
                     for i = 1:size(app.results.permutation_network_test_results, 2)
                         result = app.results.permutation_network_test_results{i};
@@ -133,14 +133,14 @@ classdef NLAResult < matlab.apps.AppBase
                         app.createNode(root, 'Non-permuted', {result, flags});
                     end
                     
-                    if app.net_input_struct.full_conn && result.has_full_conn
+                    if app.net_input_struct.full_connectome && result.has_full_conn
                         perm_result = app.results.permutation_network_test_results{i};
-                        if app.net_input_struct.full_conn && ~isequal(result.full_connectome, false)
+                        if app.net_input_struct.full_connectome && ~isequal(result.full_connectome, false)
                             flags = struct();
                             flags.show_full_conn = true;
                             app.createNode(root, 'Full connectome', {perm_result, flags});
                         end
-                        if app.net_input_struct.within_net_pair && ~isequal(result.within_network_pair, false)
+                        if app.net_input_struct.within_network_pair && ~isequal(result.within_network_pair, false)
                             flags = struct();
                             flags.show_within_net_pair = true;
                             app.createNode(root, 'Within Net-pair', {perm_result, flags});
@@ -381,7 +381,7 @@ classdef NLAResult < matlab.apps.AppBase
             
             net_results = app.test_pool.runNetTests(app.net_input_struct, app.edge_result, app.input_struct.net_atlas, false);
             
-            if app.net_input_struct.full_conn
+            if app.net_input_struct.full_connectome
                 prog.Message = 'Starting parallel pool...';
                 prog.Value = 0.05;
                 
