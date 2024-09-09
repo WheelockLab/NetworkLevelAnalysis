f9c1b8a3f9d58f226b6e0128e1546335d6d032f7
Jim Pollaro <jim.pollaro@gmail.com>
2024-09-09 15:51:28 -0500

    nodes seems to work

--- +nla/+edge/+permutationMethods/+tree/PermutationTree.m
+++ +nla/+edge/+permutationMethods/+tree/PermutationTree.m
@@ -2,37 +2,17 @@ classdef PermutationTree < handle
 
     properties
         permutation_groups
-        data_with_indexes = [] % This is going to be three values [data value, original index, current index]
-        children = []
     end
 
-    properties (SetAccess = immutable)
-        original_input_data
+    properties (setAccess = immutable)
+        original_data % This is going to be a matrix of input_data.length x 3 [data_value, current_index_in_group, original_index]
     end
 
     methods
-        function obj = PermutationTree(permutation_groups, input_data)
+        function obj = PermutationTree(input_data, permutation_groups)
             obj.permutation_groups = permutation_groups;
-            obj.original_input_data = input_data;
-            for index = 1:numel(input_data(:, 1))
-                size_input_data = size(input_data);
-                if size_input_data(:, 2) == 1 % if data input is original single column
-                    obj.data_with_indexes(end+1, :) = [input_data(index), index, index];
-                else % if data is another tree
-                    obj.data_with_indexes(end+1, :) = [input_data(index, 1), input_data(index, 2), index];
-                end
-            end
-            obj = obj.createTree();
-        end
-
-        function obj = createTree(obj)
-            groups = unique(obj.permutation_groups(:, 1));
-            if size(obj.permutation_groups, 2) > 1
-                for group = 1:numel(groups)
-                    group_indexes = find(obj.permutation_groups(:, 1) == group);
-                    new_tree = PermutationTree(obj.permutation_groups(group_indexes, 2:end), obj.data_with_indexes(group_indexes, :));
-                    obj.children = [obj.children; new_tree];
-                end
+            for index = 1:numels(input_data)
+                obj.original_data(:, end + 1) = [input_data(index), index, index];
             end
         end
     end
