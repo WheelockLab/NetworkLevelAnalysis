91526003759f597dd9e31723f90bbb9ef8ac0c89
Jim Pollaro <jim.pollaro@gmail.com>
2024-09-12 13:11:50 -0500

    Pretty sure that multi-level is working

--- +nla/+edge/+permutationMethods/+tree/PermutationTree.m
+++ +nla/+edge/+permutationMethods/+tree/PermutationTree.m
@@ -2,18 +2,18 @@ classdef PermutationTree < handle
 
     properties
         permutation_groups
+        root_node
     end
 
-    properties (setAccess = immutable)
-        original_data % This is going to be a matrix of input_data.length x 3 [data_value, current_index_in_group, original_index]
+    properties (SetAccess = immutable)
+        original_data = {} % This is going to be a matrix of input_data.length x 2 [data_value, original_index]
     end
 
     methods
         function obj = PermutationTree(input_data, permutation_groups)
+            obj.original_data = {input_data, [1:size(input_data, 2)]'};
             obj.permutation_groups = permutation_groups;
-            for index = 1:numels(input_data)
-                obj.original_data(:, end + 1) = [input_data(index), index, index];
-            end
+            obj.root_node = nla.edge.permutationMethods.tree.PermutationNode(0, input_data, permutation_groups);
         end
     end
 end
