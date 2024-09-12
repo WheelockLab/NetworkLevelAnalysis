0de3e9e56b8cfa3169433103e684b50e874cd200
Jim Pollaro <jim.pollaro@gmail.com>
2024-09-12 13:11:50 -0500

    Pretty sure that multi-level is working

--- +nla/+edge/+permutationMethods/+tree/PermutationNode.m
+++ +nla/+edge/+permutationMethods/+tree/PermutationNode.m
@@ -1,10 +1,11 @@
 classdef PermutationNode < handle
 
+
     properties
         children = []
         level = 0 % 0 is root, initial_data
         parent = false % if false, this is the root of the tree
-        data_with_indexes = []
+        data_with_indexes = {}
         permutation_groups = []
     end
 
@@ -14,20 +15,23 @@ classdef PermutationNode < handle
 
     methods
         function obj = PermutationNode(level, input_data, permutation_groups)
+            % Inputs:
+            % input_data is the functional connectivity with each subject as a vector. Size: [edges(?) x subjects]
+            % permutation groups. This will be a matrix with each level of permutation a vector. Size: [subjects x levels of permutations]
+
             if isequal(level, 0)
                 obj.permutation_groups = permutation_groups;
-                for index = 1:numel(input_data(:, 1))
-                    obj.original_data(end + 1, :) = [input_data(index), index, index];
-                end
+                obj.original_data = {input_data, [1:size(input_data, 2)'], [1:size(input_data, 2)']};
                 obj.data_with_indexes = obj.original_data;
             else
                 size_permutation_groups = size(permutation_groups);
                 if size_permutation_groups(2) > 1
                     obj.permutation_groups = permutation_groups(:, 2:end);
                 end
-                for index = 1:numel(input_data(:, 1))
-                    obj.original_data(end + 1, :) = [input_data(index, 1), input_data(index, 3), input_data(index, 3)];
-                end
+                functional_connectivity = input_data{1};
+                current_index = input_data{2};
+                original_index = input_data{3};
+                obj.original_data = {functional_connectivity, current_index', original_index'};
                 obj.data_with_indexes = obj.original_data;
             end
             if ~isempty(obj.permutation_groups)
@@ -35,7 +39,10 @@ classdef PermutationNode < handle
                 for group_number = 1:numel(group_numbers)
                     group_indexes = (obj.permutation_groups(:, 1) == group_numbers(group_number));
                     temp_permutation_groups = obj.permutation_groups(group_indexes, :);
-                    group_data = obj.data_with_indexes(group_indexes, :);
+                    group_input_data = obj.data_with_indexes{1};
+                    group_current_indexes = obj.data_with_indexes{2};
+                    group_original_indexes = obj.data_with_indexes{3};
+                    group_data = {group_input_data(:, group_indexes), group_current_indexes(group_indexes), group_original_indexes(group_indexes)};
                     obj.children = [obj.children obj.createNodes(obj.level + 1, temp_permutation_groups, group_data, obj)];
                 end
             end
--- +nla/+edge/+permutationMethods/MultiLevel.m
+++ +nla/+edge/+permutationMethods/MultiLevel.m
@@ -1,16 +1,62 @@
-classdef MultiLevel < handle
+classdef MultiLevel < nla.edge.permutationMethods.Base
 
     properties
-        test_options
+        permutation_tree
+        terminal_nodes = []
     end
 
     methods
-        function obj = MultiLevel(test_options)
-            obj.test_options = test_options;
+        function obj = MultiLevel()
         end
 
-        function permuted = permute(obj)
+        function obj = createPermutationTree(obj, input_struct)
+            if ~isfield(input_struct, "permutation_groups")
+                input_struct.permutation_groups = [];
+            end
+            obj.permutation_tree = nla.edge.permutationMethods.tree.PermutationTree(...
+                input_struct.func_conn.v, input_struct.permutation_groups...
+            );
+            tree_root = obj.permutation_tree.root_node;
+            obj = obj.depthFirstSearch(tree_root);
+        end
 
+        function permuted_input_struct = permute(obj, orig_input_struct)
+            permuted_input_struct = orig_input_struct;
+            transposed_functional_connectivity = orig_input_struct.func_conn.v';
+            permuted_transposed_functional_connectivity = zeros(size(transposed_functional_connectivity));
+
+            for node_index = 1:numel(obj.terminal_nodes)
+                node = obj.terminal_nodes(node_index);
+                current_indexes = node.data_with_indexes{2};
+                original_indexes = node.original_data{3};
+                % start_index = original_indexes(1);
+                % end_index = original_indexes(end);
+
+                permuted_indexes = nla.helpers.permuteVector(current_indexes);
+                node.data_with_indexes{2} = permuted_indexes;
+                permuted_transposed_functional_connectivity(permuted_indexes, :) = transposed_functional_connectivity(current_indexes, :);;
+            end
+
+            permuted_input_struct.func_conn.v = permuted_transposed_functional_connectivity';
+        end
+
+        function obj = depthFirstSearch(obj, node_input)
+            node_queue = [node_input];
+            while ~isempty(node_queue)
+                node = node_queue(1);
+                if isempty(node.children)
+                    obj.terminal_nodes = [obj.terminal_nodes, node];
+                else
+                    for child = 1:numel(node.children)
+                        node_queue = [node_queue; node.children(child)];
+                    end
+                end
+                if size(node_queue, 1) > 1
+                    node_queue = node_queue(2:end, :);
+                else
+                    node_queue = [];
+                end
+            end
         end
     end
 end
