e3783bdffc6dd609bd29641eb1001d13b8f59a0f
Jim Pollaro <jim.pollaro@gmail.com>
2024-09-13 10:55:59 -0500

    slight change to make matrix sizes work. added documentation

--- +nla/+edge/+permutationMethods/+tree/PermutationTree.m
+++ +nla/+edge/+permutationMethods/+tree/PermutationTree.m
@@ -1,8 +1,20 @@
 classdef PermutationTree < handle
+    %PERMUTATIONTREE Base class for permutation groups. This just bundles all the permutation nodes
+    % together for ease. 
+    %
+    % tree = PermutationTree(input_data, permutation_groups)
+    % tree = The tree object. Consists of the raw permutation groups along with the raw data along
+    %   starting indexes
+    % 
+    % input_data = The data that is permuted. This is usually the functional connectivity (right now, that's all it works for)
+    % permutation_groups = the columns from the csv file that characterizes the permutation groupings
+    %   Each column of the data is 1 permutation grouping. 
+    %   Each row is one subject/data entry
+    %   Values should be positive integers. Groups must be independent in each column
 
     properties
-        permutation_groups
-        root_node
+        permutation_groups % Raw from the behavior table to define permutation grouping
+        root_node % The first "level 0" node of the tree
     end
 
     properties (SetAccess = immutable)
--- +nla/+edge/+permutationMethods/MultiLevel.m
+++ +nla/+edge/+permutationMethods/MultiLevel.m
@@ -1,4 +1,22 @@
 classdef MultiLevel < nla.edge.permutationMethods.Base
+    %MULTILEVEL Multilevel permutation method
+    %
+    % multi-level strategy = multiLevel()
+    % multi-level strategy = the object which will be doing the permutations the data. The functional
+    %   connectivity is permuted
+    % 
+    % needed for methods:
+    % test_options = this is the options and data for the tests being run. Sometimes called 'input_struct'
+    %
+    % How this works: THe object controls the permutations. It can be instantiated without any data
+    % Before permutations can be run, a permutation tree needs to be created. This needs to be done with the
+    % "createPermutationTree(test_options)" method. This creates a permutation tree (with the nodes). It
+    % also will find all the "terminal nodes" - the nodes that have no children. These are the last groupings
+    % and the actual data to be permuted
+    %
+    % After the tree is created (this only is done once), the object is ready to do the permutations.
+    % This is done by taking each terminal node, getting the current indexes of the data, permuting them, 
+    % and then applying these indexes to the data.
 
     properties
         permutation_tree
@@ -22,19 +40,20 @@ classdef MultiLevel < nla.edge.permutationMethods.Base
 
         function permuted_input_struct = permute(obj, orig_input_struct)
             permuted_input_struct = orig_input_struct;
-            transposed_functional_connectivity = orig_input_struct.func_conn.v';
-            permuted_transposed_functional_connectivity = zeros(size(transposed_functional_connectivity));
+            permuted_transposed_functional_connectivity = zeros(size(orig_input_struct.func_conn.v'));
 
             for node_index = 1:numel(obj.terminal_nodes)
                 node = obj.terminal_nodes(node_index);
                 current_indexes = node.data_with_indexes{2};
-                original_indexes = node.original_data{3};
-                % start_index = original_indexes(1);
-                % end_index = original_indexes(end);
-
+                original_indexes = node.data_with_indexes{3};
+                data = node.data_with_indexes{1}'; % transpose to match dimensions of indexes
+                
                 permuted_indexes = nla.helpers.permuteVector(current_indexes);
-                node.data_with_indexes{2} = permuted_indexes;
-                permuted_transposed_functional_connectivity(permuted_indexes, :) = transposed_functional_connectivity(current_indexes, :);;
+                sorted_original_indexes = sort(original_indexes, 2);
+                original_indexes = original_indexes(permuted_indexes);
+                data = data(permuted_indexes, :);
+                node.data_with_indexes = {data', current_indexes, original_indexes};
+                permuted_transposed_functional_connectivity(sorted_original_indexes, :) = data;
             end
 
             permuted_input_struct.func_conn.v = permuted_transposed_functional_connectivity';
