f17475cc61885fa66befd9a0ea248d978cc3197d
Jim Pollaro <jim.pollaro@gmail.com>
2025-01-22 09:27:59 -0600

    code monkey commented out wrong lines

--- +nla/+net/+result/NetworkTestResult.m
+++ +nla/+net/+result/NetworkTestResult.m
@@ -77,21 +77,21 @@ classdef NetworkTestResult < matlab.mixin.Copyable
         function output(obj, edge_test_options, updated_test_options, edge_test_result, flags)
             % This function is here to run plots in scripting. GUI doesn't call this.
 
-            % nla.net.result.plot.NetworkTestPlotApp(obj, edge_test_result, flags, edge_test_options,...
-            %     updated_test_options);
+            nla.net.result.plot.NetworkTestPlotApp(obj, edge_test_result, flags, edge_test_options,...
+                updated_test_options);
 
             % If the above doesn't work in scripts, then uncomment below and use it
-            if isfield(flags, "show_nonpermuted") && flags.show_nonpermuted
-                test_method = "no_permutations";
-            elseif isfield(flags, "show_full_conn") && flags.show_full_conn
-                test_method = "full_connectome";
-            elseif isfield(flags, "show_within_net_pair") && flags.show_within_net_pair
-                test_method = "within_network_pair";
-            end
+            % if isfield(flags, "show_nonpermuted") && flags.show_nonpermuted
+            %     test_method = "no_permutations";
+            % elseif isfield(flags, "show_full_conn") && flags.show_full_conn
+            %     test_method = "full_connectome";
+            % elseif isfield(flags, "show_within_net_pair") && flags.show_within_net_pair
+            %     test_method = "within_network_pair";
+            % end
 
-            network_result_plot = nla.net.result.plot.NetworkTestPlot(obj, edge_test_result, edge_test_options.net_atlas,...
-                test_method, edge_test_options, updated_test_options);
-            network_result_plot.drawFigure(nla.PlotType.FIGURE)
+            % network_result_plot = nla.net.result.plot.NetworkTestPlot(obj, edge_test_result, edge_test_options.net_atlas,...
+            %     test_method, edge_test_options, updated_test_options);
+            % network_result_plot.drawFigure(nla.PlotType.FIGURE)
         end
 
         function merge(obj, other_objects)
